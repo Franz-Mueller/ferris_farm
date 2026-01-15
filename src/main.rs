3848caea3ea7914b55269c7929fbeec4a3435b99
Franz-Mueller <franz@franzhenrymueller.de>
2026-01-15 21:09:54 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use ferris_farm::ThreadPool;
+use ferris_farm::http::threads::ThreadPool;
 use std::{
     fs,
     io::{BufReader, prelude::*},
@@ -11,7 +11,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming().take(100) {
         let stream = stream.unwrap();
 
         pool.execute(|| {

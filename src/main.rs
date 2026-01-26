980ee2e368dedea0ea6f60a72eb193dc63e60d63
Franz Müller <105726151+Franz-Mueller@users.noreply.github.com>
2026-01-26 22:10:12 +0100

    redo project (#18)
    * redo project
    
    * fix
    
    * fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("lol");
+use ferris_farm::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000");
+    run(listener)?.await
 }

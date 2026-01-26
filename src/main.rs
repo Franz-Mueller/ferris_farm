312abc2bef8fc374c76f3f555b941a49118a0813
Franz-Mueller <franz@franzhenrymueller.de>
2026-01-26 21:53:33 +0100

    redo project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("lol");
+use ferris_farm::run;
+use std::net::TcpListener;
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000");
+    run(listener)?.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,27 @@
+use reqwest;
+use std::net::TcpListener;
+use tokio;
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    // Act
+    let response = client
+        .get("{}/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = ferris_farm::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
